a39fec519cadfc9c944fc376ad11556c3f14ae55
Bijendra Nayak <bijendranayak@Bijendras-MacBook-Pro.local>
2017-06-19 22:05:42 +0530

    nfcontrol2

--- src/NFIntegration/nformBundle/Controller/NFWebhookController.php
+++ src/NFIntegration/nformBundle/Controller/NFWebhookController.php
@@ -18,11 +18,11 @@ class NFWebhookController extends Controller
     	
     	$f = fopen('/tmp/hook.log', 'a+');
     	$d = date('Y-m-d H:i:s');
-
+    	fwrite($f, "---------  DATETIME: $d -----------\n");
     	$data = json_decode(file_get_contents('php://input'));
     	fwrite($f, print_r($data, true));
     	fclose($f);
-        
+        fwrite($f, "------------- END -----------\n");
         return $this->render('NFIntegrationnformBundle:Default:index.html.twig');
     }
 }
