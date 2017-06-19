e6683f14358d27bede77ae704aab6feb84eaac89
Bijendra Nayak <bijendranayak@Bijendras-MacBook-Pro.local>
2017-06-19 18:04:53 +0530

    2nd commit

--- src/NFIntegration/nformBundle/Controller/NFWebhookController.php
+++ src/NFIntegration/nformBundle/Controller/NFWebhookController.php
@@ -15,10 +15,14 @@ class NFWebhookController extends Controller
     public function indexAction()
     {
 
-
-    	$f = fopen('/tmp/hook.log', 'a+');
+    	/*
+    	$f = fopen('/Applications/MAMP/htdocs/bijen/hook.log', 'a+');
     	$d = date('Y-m-d H:i:s');
-        
+
+    	$data = json_decode(file_get_contents('php://input'));
+    	fwrite($f, print_r($data, true));
+    	fclose($f);
+        */
         return $this->render('NFIntegrationnformBundle:Default:index.html.twig');
     }
 }
