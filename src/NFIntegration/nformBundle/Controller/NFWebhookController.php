6c333b4c349dcd1b4077cf1db3cf1a71b149a9e4
Bijendra Nayak <bijendranayak@Bijendras-MacBook-Pro.local>
2017-06-20 00:17:15 +0530

    control

--- src/NFIntegration/nformBundle/Controller/NFWebhookController.php
+++ src/NFIntegration/nformBundle/Controller/NFWebhookController.php
@@ -18,19 +18,22 @@ class NFWebhookController extends Controller
      */
     public function indexAction(Request $request)
     {
-    	
-    	$fs = new Filesystem();	
+    	/*
+    	$fs = new Filesystem('/tmp/nook.log');	
     	//$dt= date('Y-m-d H:i:s');
     	//$data = json_decode(file_get_contents('php://input'));
 		//$fs->touch('hook.log');
+		$data= 12334;
+		$file="dhsbhasa";
 
-		$data= $request->get('documentNumber');
-		$file = $request->get('files');
-    	//$fs->dumpFile( 'tmp/hook.log', $dt);
-		$fs->dumpFile( 'tmp/hook.log', $data);	
-		$fs->dumpFile( 'tmp/hook.log', $file);	
+		//$data= $request->get('documentNumber');
+		//$file = $request->get('files');
+    	//$fs->dumpFile( './tmp/hook.log', $dt);
+    	//file_put_contents('tmp/nook.log', $data, FILE_APPEND);
+		$fs->dumpFile( 'nook.log', $data);	
+		$fs->dumpFile( 'nook.log', $file);	*/
 
-    	/*
+    
     	$f = fopen('tmp/hook.log', 'a+');
     	$date= date('Y-m-d H:i:s');
     	fwrite($f, "---------  DATETIME: $d -----------\n");
@@ -38,7 +41,7 @@ class NFWebhookController extends Controller
     	fwrite($f, print_r($data, true));
     	fclose($f);
         fwrite($f, "------------- END -----------\n");
-       */
+      
         return $this->render('NFIntegrationnformBundle:Default:index.html.twig');
     }
 }
